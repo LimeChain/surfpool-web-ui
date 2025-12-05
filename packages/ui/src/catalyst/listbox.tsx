4db6cc736e4e5ad7df9a14102a11cf303f07a13c
Ludo Galabru <ludo@txtx.builders>
2025-12-04 20:12:08 -0600

    fix: timeTravel popup

--- packages/ui/src/catalyst/listbox.tsx
+++ packages/ui/src/catalyst/listbox.tsx
@@ -82,7 +82,7 @@ export function Listbox<T>({
           // Anchor positioning
           '[--anchor-offset:-1.625rem] [--anchor-padding:--spacing(4)] sm:[--anchor-offset:-1.375rem]',
           // Base styles
-          'isolate w-max min-w-[calc(var(--button-width)+1.75rem)] scroll-py-1 rounded-xl p-1 select-none',
+          'isolate z-50 w-max min-w-[calc(var(--button-width)+1.75rem)] scroll-py-1 rounded-xl p-1 select-none',
           // Invisible border that is only visible in `forced-colors` mode for accessibility purposes
           'outline outline-transparent focus:outline-hidden',
           // Handle scrolling when menu won't fit in viewport
